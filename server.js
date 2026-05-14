e84531bd7c612f29c0a730b6e081d3847299c05e
Maksym Shchurko <maksym35352@example.com>
2026-05-14 04:01:14 +0300

    Introduce error for CI test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/donors', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running on http://localhost:3000";
 });
 
 // Для перевірки Pull Request
